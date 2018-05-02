eff90041aa27873fe2f49d0fab8445dcc9d0b275
Max-Nils Brüschke <max.brueschke@plentymarkets.com>
2018-05-02 12:18:23 +0200

    Update TracdelightCOM.php

--- src/ResultField/TracdelightCOM.php
+++ src/ResultField/TracdelightCOM.php
@@ -106,7 +106,7 @@ class TracdelightCOM extends ResultFields
         /**
          * @var LanguageMutator $languageMutator
          */
-		$languageMutator = pluginApp(LanguageMutator::class, ['language' => [$settings->get('lang')]]);
+		$languageMutator = pluginApp(LanguageMutator::class, ['languages' => [$settings->get('lang')]]);
 
 		/**
 		 * @var KeyMutator
